d9c8dc5c3eca66e490c6281b197477f3707f7dc6
Nathan S. Loveless <nloveles@norwich.edu>
2020-02-10 16:51:32 -0500

    Working on a few errors in model .js

--- projects/projects_model.js
+++ projects/projects_model.js
@@ -1,3 +1,51 @@
-const db = require('../projects/projects_router');
+const db = require('../data/db-config');
+
+function findResources() {
+     return db('resources');
+}
+
+function findProjects() {
+    return db('projects');
+
+}
+
+function findTasks() {
+    return db('tasks');
+    // const tasks =  db('tasks');
+
+    // tasks.map(task => {
+    //     const projId = db('projects_tasks').where('task_id', task.id);
+    //      const project = db('projects').where('id', projId);
+    //      task.project = project.name;
+    //      task.project_description = project.description; 
+    // })
+
+    // return tasks;
+    //return db('steps as st').join('schemes as sc').select('st.id', 'sc.scheme_name', 'st.step_number', 'st.instructions').where('st.scheme_id', id).where('sc.id', id).orderBy('step_number');
+}
+
+function addResource(res) {
+    return db('resources').insert(res)
+
+}
+
+function addProject(proj) {
+    return db('projects').insert(proj)
+}
+
+function addTask(task) {
+    return db('tasks').insert(task)
+}
+
+
+
+module.exports = {
+    findResources,
+    findProjects,
+    findTasks,
+    addResource,
+    addProject,
+    addTask
+}
 
 
--- projects/projects_router.js
+++ projects/projects_router.js
@@ -4,5 +4,66 @@ const Projects = require('./projects_model');
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+  Projects.findProjects()
+  .then(proj => {
+    res.json(proj);
+  })
+  .catch(err => {
+    res.status(500).json({ message: 'Failed to get resources' });
+  });
+});
+
+router.get('/resources', (req, res) => {
+  Projects.findResources()
+  .then(resource => {
+    res.json(resource);
+  })
+  .catch(err => {
+    res.status(500).json({ message: 'Failed to get resources' });
+  });
+});
+
+router.get('/tasks', (req, res) => {
+  Projects.findTasks()
+  .then(task => {
+    res.json(task);
+  })
+  .catch(err => {
+    res.status(500).json({ message: 'Failed to get resources' });
+  });
+});
+
+router.post('/resources', (req, res) => {
+  Projects.addResource(req.body)
+  .then(resource => {
+    res.json(resource);
+  })
+  .catch(err => {
+    res.status(500).json({ message: 'Failed to get resources' });
+  });
+});
+
+router.post('/', (req, res) => {
+  Projects.addProject(req.body)
+  .then(proj => {
+    res.json(proj);
+  })
+  .catch(err => {
+    res.status(500).json({ message: 'Failed to get resources' });
+  });
+});
+
+router.post('/tasks', (req, res) => {
+  Projects.addTask(req.body)
+  .then(task => {
+    res.json(task);
+  })
+  .catch(err => {
+    res.status(500).json({ message: 'Failed to get resources' });
+  });
+});
+
+
 
   module.exports = router;
